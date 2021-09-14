b62474678bd6a4c0f159eeb931501f53f4648374
Luis Jauregui <luis.jauregui.fl@gmail.com>
2021-09-14 18:32:14 -0500

    project initializated in cli

---
+++ Inmobiliaria/src/components/generals.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Layout = styled.view`
+  background-color: #ffffff;
+  height: 100%;
+  min-height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ItemContainer = styled.view`
+  height: 20%;
+  min-height: 20%;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+`;
---
+++ Inmobiliaria/src/components/index.js
@@ -0,0 +1,1 @@
+export { ItemContainer, Layout } from "./generals";
